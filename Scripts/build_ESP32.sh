ce7e324e9eb7dc5c4c1bbc3621166ae38f0a3ca5
hredan <hredan@sleepuino.de>
2026-05-10 07:53:52 +0200

    update Build Scripts

--- Scripts/build_ESP32.sh
+++ Scripts/build_ESP32.sh
@@ -9,11 +9,12 @@ CORE="esp32"
 BOARD="d1_mini32"
 CORE_VERSION=""
 CPU_F="160"
+LIBS="Dictionary"
 
 URL=$(git remote get-url origin)
 HASH=$(git rev-parse HEAD)
 
 
-$BUILD_ACTION_DIR/build_sketch.sh -s $SKETCH_NAME -c $CORE -b $BOARD -f $CPU_F
+$BUILD_ACTION_DIR/build_sketch.sh -s $SKETCH_NAME -c $CORE -b $BOARD -f $CPU_F -l $LIBS
 $BUILD_ACTION_DIR/create_build_info.sh -r $URL -s $HASH
 $BUILD_ACTION_DIR/create_eep_package.sh -s $SKETCH_NAME -c $CORE -b $BOARD
--- Scripts/build_ESP8266.sh
+++ Scripts/build_ESP8266.sh
@@ -4,16 +4,17 @@
 # With GitBash the scripts are working also on Windows.
 
 BUILD_ACTION_DIR="../eep-build-action/"
-SKETCH_NAME="ESP_Blink"
+SKETCH_NAME="ESP_HELLO_WORLD"
 CORE="esp8266"
 BOARD="d1_mini"
 CORE_VERSION=""
 CPU_F="80"
+LIBS="Dictionary"
 
 URL=$(git remote get-url origin)
 HASH=$(git rev-parse HEAD)
 
 
-$BUILD_ACTION_DIR/build_sketch.sh -s $SKETCH_NAME -c $CORE -b $BOARD -f $CPU_F
+$BUILD_ACTION_DIR/build_sketch.sh -s $SKETCH_NAME -c $CORE -b $BOARD -f $CPU_F -l $LIBS
 $BUILD_ACTION_DIR/create_build_info.sh -r $URL -s $HASH
 $BUILD_ACTION_DIR/create_eep_package.sh -s $SKETCH_NAME -c $CORE -b $BOARD
